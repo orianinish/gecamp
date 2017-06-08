ebac6a39472b53ce3717c8d23c1b22a83f37b8a5
wowanamatsu <antonioflavionte@gmail.com>
2017-06-08 01:04:04 -0300

    33

--- gecamp/wsgi.py
+++ gecamp/wsgi.py
@@ -1,17 +1,16 @@
 """
-WSGI config for gettingstarted project.
+WSGI config for gecamp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gecamp.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
